5acc0ebed6d92219f3456aed25926d8649deefc8
caizhaoxin <chinese19@126.com>
2021-11-14 17:03:14 +0800

    完成自动化！

--- trace/trace.js
+++ trace/trace.js
@@ -35,33 +35,41 @@ function get_instance(cls_name) {
 }
 // 开始trace debug的方法  这里暂时简单点硬编码doshare
 function trace_hook_onclick(cls_name) {
+    send({'method': 'get_cls_met_name'}); // 发送空方法，促发
+    var cls_met_name = ''
+    var trace_file_name = ''
+    recv(function (received_json_object) {
+        cls_met_name = received_json_object['cls_met_name']
+        trace_file_name = received_json_object['trace_file_name']
+        console.log('hook', cls_met_name, 'successfully');
+    }).wait(); //收到数据之后，再执行下去
     // hook onclick
-    Java.use(cls_name).onClick.implementation = function (view) {
+    Java.use(cls_met_name).onClick.implementation = function (view) {
+        console.log('hooking ', cls_met_name, ' now')
         console.log('-------------------------start trace-----------------------------')
-        debug_start_trace('czx_test')
+        console.log('hook', trace_file_name, '   trace_file_name');
+        debug_start_trace(trace_file_name)
         this.onClick(view)
         debug_stop_trace()
+        send({'method': 'pull_trace'});
+        recv(function (received_json_object) {}).wait(); //同步阻塞
         console.log('-------------------------stop trace-----------------------------')
     }
 }
 // 停止trace debug的方法  这里暂时简单点硬编码doshare
 function stop_trace(cls_name) {
     Java.use(cls_name).a.overload('java.lang.String', 'boolean').implementation = function (str, z) {
-        console.log('-------------------------stop trace-----------------------------')
+        console.log('--------------------------stop trace-----------------------------')
         // debug_stop_trace()
         return this.a(str, z)
     }
 }
 // 开始Debug的tarce    Debug.startMethodTracing("log-"+MyDate.getDataStr());
-function debug_start_trace(file_name) {
-    var timestamp = Date.parse(new Date());
+function debug_start_trace(trace_file_name) {
     var Debug = Java.use('android.os.Debug')
-    // 单位 -> 字节， 设得太小会无法缓存
     var buffsize = 102400000
-    if (file_name == '')
-        Debug.startMethodTracing('default_trace-' + timestamp, buffsize)
-    else
-        Debug.startMethodTracing(file_name + '-' + timestamp, buffsize)
+    // 单位 -> 字节， 设得太小会无法缓存
+    Debug.startMethodTracing(trace_file_name, buffsize)
 }
 // 停止Debug的tarce    Debug.stopMethodTracing();
 function debug_stop_trace() {
@@ -73,8 +81,7 @@ setTimeout(function () { //prevent timeout
     console.log("[*] Starting script");
     var onclick_cls_name = 'com.ssports.mobile.video.share.-$$Lambda$ShareDialog$ozr0h1Q2S_Y5_ihq7DdwElE4Oy0'
     Java.perform(function () {
-        var trace_start_cls_name = onclick_cls_name
-        trace_hook_onclick(trace_start_cls_name)
+        trace_hook_onclick(onclick_cls_name)
         console.log('----------hook成功，可以开始点击！！！-------------')
     })
 }, 3000)
--- trace/trace.py
+++ trace/trace.py
@@ -1,8 +1,46 @@
 import time
 import frida
+from datetime import datetime
+import os
 
 apk_name = 'com.ssports.mobile.video'
+cls_met_name = 'com.ssports.mobile.video.share.-$$Lambda$ShareDialog$ozr0h1Q2S_Y5_ihq7DdwElE4Oy0'
 
+# 运行时参数，别动！
+dt_ms = ''
+#############
+
+def set_dt_ms():
+    global dt_ms
+    dt_ms = "{}_{}".format(apk_name, datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')) # 含微秒的日期时间，来源 比特量化
+    dt_ms = dt_ms.replace('.', '_')
+    print(dt_ms)
+
+def method_select(message, payload):
+    data = message['payload']
+    type = data['method']
+    if type=='get_cls_met_name':
+        get_cls_met_name()
+    elif type=='pull_trace':
+        pull_trace()
+
+def get_cls_met_name():
+    # print(message)
+    data = {'cls_met_name': cls_met_name,
+            'trace_file_name': dt_ms}
+    script.post(data)  # 将JSON对象发送回去
+
+def pull_trace():
+    print('pulling trace file')
+    os.system('adb pull /sdcard/Android/data/{}/files/{}.trace .'.format(apk_name, dt_ms))
+    print('creating trace txt')
+    os.system('java -jar trace2txt.jar -s {}.trace -t {}.txt -f'.format(dt_ms, dt_ms))
+    print('done!')
+    # print('adb pull /sdcard/Android/data/{}/files/{}.trace .'.format(apk_name, dt_ms))
+    script.post({})  # 将JSON对象发送回去
+    # trace_file_name = str.replace('.', '_')
+
+set_dt_ms()
 device = frida.get_usb_device()
 pid = device.spawn([apk_name])
 device.resume(pid)
@@ -11,5 +49,14 @@ time.sleep(1)
 session = device.attach(pid)
 with open("trace.js",'r',encoding='UTF-8') as f:
     script = session.create_script(f.read())
+script.on("message", method_select)  # 注册消息处理函数
 script.load()
+
+
+# os.system('adb pull /sdcard/Android/data/com.ssports.mobile.video/files/aaa.trace .')
 input()
+
+
+
+
+
