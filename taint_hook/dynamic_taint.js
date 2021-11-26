b46c109ced62bd4d7dc82663d8548d4844c274a6
Wu <825583681@qq.com>
2021-11-26 23:13:31 +0800

    修改了核心算法,现在用的是Hash的方法来taint

--- taint_hook/dynamic_taint.js
+++ taint_hook/dynamic_taint.js
@@ -1,7 +1,11 @@
+
 //-------------------------util--------------------------------
 
 //分享的内容
 var global_shareContent = []
+var hash_taint_list = []
+var function_trace = []
+var taint_count = 0
 
 // 打印调用栈
 function printStack() {
@@ -143,7 +147,123 @@ function hook(targetClass, targetReturn, targetMethod, targetArguments) {
 
         overloads[i].implementation = function () {
             console.warn("\n*** entered " + targetClassMethod);
+            console.log('targetArguments = ' + targetArgumentsArr)
+            console.log("targetReturn = " + targetReturn)
+            //照常返回，不对返回值做操作
+            var retval = this[targetMethod].apply(this, arguments);
+            
+            //taint_index是用来标记当前参数匹配上了哪个hash_taint
+            var taint_index = -1
+            var is_find_new_taint = false
+            for(var j = 0; j < arguments.length; j++){
 
+                //参数类型
+                var tpye_of_argument = targetArgumentsArr[j]
+                //参数值
+                var argument = arguments[j]
+                //参数String值
+                var argument_str = argument.toString()
+                    //判断当前参数有没有分享内容
+                    for(var k = 0; k < global_shareContent.length; k++){
+                        //如果有，就说明要在列表中新开一个hash_taint，当作是一个新的分支
+                        if (argument_str.indexOf(global_shareContent[k]) != -1){
+
+                            //总的分享内容的数量
+                            taint_count++
+                            is_find_new_taint = true
+                            
+                            if (tpye_of_argument.startsWith('[')){
+                                //数组的hashCode
+                                hash_taint_list.push(argument.hashCode(argument))
+                            }else{
+                                //类的hashCode
+                                hash_taint_list.push(argument.hashCode())
+                            }
+                            //当前的新的函数路径，开启一条新的分支，每遇到一个分享内容就认为他之前没有出现过
+                            var new_source_list = []
+                            new_source_list.push(targetClassMethod)
+                            //function_trace是用来记录所有包含分享内容的分支
+                            function_trace.push(new_source_list)
+                        }
+                    }
+
+                
+                //不是基本的数据类型并且有返回值
+                if (tpye_of_argument != 'int' && tpye_of_argument != 'double' && tpye_of_argument != 'float' 
+                && tpye_of_argument != 'long' && tpye_of_argument != 'boolean' && tpye_of_argument != 'short'
+                && tpye_of_argument != 'byte' && tpye_of_argument != 'char' && tpye_of_argument != 'undefined'){
+
+                    var argu_hash = ""
+
+                    //说明是一个数组
+                    if (tpye_of_argument.startsWith('[')){
+                        //hashCode
+                        argu_hash = argument.hashCode(argument)
+                    }else{
+                        //hashCode
+                        argu_hash = argument.hashCode()
+                    }
+
+                    //不是新出现的分享内容，说明要在hash_list里面查找有没有taint
+                    if (argu_hash != "" && !is_find_new_taint){
+                        for (var k = 0;k < hash_taint_list.length; k++){
+                            //找到了说明当前的分享内容进入到了这个函数
+                            if (argu_hash == hash_taint_list[k]){
+                                //标记一下当前的位置
+                                taint_index = k
+                                //加入到function_trace中，相当于一个函数调用链
+                                function_trace[taint_index].push(targetClassMethod)
+                                break
+                            }
+                        }
+                    }
+
+                    if (taint_index != -1){
+                        if(targetReturn != 'int' && targetReturn != 'double' && targetReturn != 'float' 
+                        && targetReturn != 'long' && targetReturn != 'boolean' && targetReturn != 'short'
+                        && targetReturn != 'byte' && targetReturn != 'char' && targetReturn != 'undefined'){
+
+                            if (targetReturn.startsWith('[')){
+                                var hash_taint = retval.hashCode(retval)
+                                hash_taint_list[taint_index] = hash_taint
+                            }else{
+                                var hash_taint = retval.hashCode()
+                                hash_taint_list[taint_index] = hash_taint
+                            }
+
+                        }else{
+                            
+                            hash_taint_list[taint_index] = argu_hash
+
+                        }
+                    }
+
+                    if (argu_hash != '' && is_find_new_taint){
+                        if(targetReturn != 'int' && targetReturn != 'double' && targetReturn != 'float' 
+                        && targetReturn != 'long' && targetReturn != 'boolean' && targetReturn != 'short'
+                        && targetReturn != 'byte' && targetReturn != 'char' && targetReturn != 'undefined'){
+
+                            if (targetReturn.startsWith('[')){
+                                var hash_taint = retval.hashCode(retval)
+                                hash_taint_list[taint_count] = hash_taint
+                            }else{
+                                var hash_taint = retval.hashCode()
+                                hash_taint_list[taint_count] = hash_taint
+                            }
+
+                        }else{
+                            
+                            hash_taint_list[taint_count] = argu_hash
+
+                        }
+                    }
+
+                    
+                }
+                
+            }
+
+            return retval;
 
             //console.log(targetClassMethod)
 
@@ -153,96 +273,99 @@ function hook(targetClass, targetReturn, targetMethod, targetArguments) {
             //     console.log("\nBacktrace:\n" + bt);
             // });
 
-            if (arguments.length) console.log();
-            var taint
-            var has_taint = false
-            for (var j = 0; j < arguments.length; j++) {
-                var str = ''
+            // if (arguments.length) console.log();
+            // var taint
+            // var has_taint = false
+            // for (var j = 0; j < arguments.length; j++) {
+            //     var str = arguments[j].toString()
 
-                // String
-                if (targetArgumentsArr[j] == 'java.lang.String') {
-                    str = arguments[j]
-                }
-                // byte[]
-                else if (targetArgumentsArr[j] == '[B') {
-                    var javaString = Java.use('java.lang.String');
-                    str = javaString.$new(arguments[j]);
-                }
-                //HashMap
-                else if (targetArgumentsArr[j] == 'java.util.HashMap'){
-                    str = arguments[j].toString()
-                }
-                //JSONObject
-                else if (targetArgumentsArr[j] == 'org.json.JSONObject'){
-                    str = arguments[j].toString()
-                }
-                //JSONArray
-                else if (targetArgumentsArr[j] == 'org.json.JSONArray'){
-                    str = arguments[j].toString()
-                }
+            //     // String
+            //     if (targetArgumentsArr[j] == 'java.lang.String') {
+            //         str = arguments[j]
+            //     }
+            //     // byte[]
+            //     else if (targetArgumentsArr[j] == '[B') {
+            //         var javaString = Java.use('java.lang.String');
+            //         str = javaString.$new(arguments[j]);
+            //     }
+            //     //HashMap
+            //     else if (targetArgumentsArr[j] == 'java.util.HashMap'){
+            //         str = arguments[j].toString()
+            //     }
+            //     //JSONObject
+            //     else if (targetArgumentsArr[j] == 'org.json.JSONObject'){
+            //         str = arguments[j].toString()
+            //     }
+            //     //JSONArray
+            //     else if (targetArgumentsArr[j] == 'org.json.JSONArray'){
+            //         str = arguments[j].toString()
+            //     }
 
 
 
-                // other....
+            //     // other....
 
-                //console.log("2===============================================")
-                //查看当前传入的参数是否包含了分享的内容，循环便利分享内容数组
-                if (str != ''){
-                    for(var k = 0;k < global_shareContent.length;k++){
-                        if (str.indexOf(global_shareContent[k]) != -1) {
-                            taint = global_shareContent[k]
-                            has_taint = true
-                            console.log(str)
-                        }
-                    }
-                }
+            //     //console.log("2===============================================")
+            //     //查看当前传入的参数是否包含了分享的内容，循环便利分享内容数组
+            //     if (str != ''){
+            //         for(var k = 0;k < global_shareContent.length;k++){
+            //             if (str.indexOf(global_shareContent[k]) != -1) {
+            //                 taint = global_shareContent[k]
+            //                 has_taint = true
+            //                 console.log('str = ' + str)
+            //             }
+            //         }
+            //     }
                 
-            }
+            // }
 
             //console.log("3==========================================")
             //打印返回值
-            var retval = this[targetMethod].apply(this, arguments); // rare crash (Frida bug?)
+             // rare crash (Frida bug?)
 
-            if(has_taint && "http" in targetMethod.toLowerCase() && "post" in targetMethod.toLowerCase()){
-                console.log("这个app是阳性")
-            }
+            //console.log("byte to string:" + byteToString(retval))
+
+            // if(has_taint && "http" in targetMethod.toLowerCase() && "post" in targetMethod.toLowerCase()){
+            //     console.log("这个app是阳性")
+            // }
 
             //console.log("4==========================================")
             // 处理返回值
             // 有污点
             // console.log(has_taint)
-            if (has_taint) {
-                if (targetReturn == 'java.lang.String') {
-                    retval += taint
-                }
-                if (targetReturn == '[B') {
-                    var javaString = Java.use('java.lang.String')
-                    var str = javaString.$new(retval);
-                    str += taint
-                    console.log('str'+str)
-                    retval = Java.array('byte', stringToByte(str));
-                }
-                //如果返回值是HashMap，那么就加多一对key value，key是gosec，value是分享的内容
-                if (targetReturn == 'java.util.HashMap'){
-                    retval.put('gosec!!', taint)
-                }
+            // if (has_taint) {
+            //     if (targetReturn == 'java.lang.String') {
+            //         retval += taint
+            //     }
+            //     if (targetReturn == '[B') {
+            //         var javaString = Java.use('java.lang.String')
+            //         var str = javaString.$new(retval);
+            //         str += taint
+            //         console.log('str'+str)
+            //         retval = Java.array('byte', stringToByte(str));
+            //     }
+            //     //如果返回值是HashMap，那么就加多一对key value，key是gosec，value是分享的内容
+            //     if (targetReturn == 'java.util.HashMap'){
+            //         retval.put('gosec!!', taint)
+            //     }
 
-                if (targetReturn == 'org.json.JSONObject'){
-                    retval.put('gosec!!', taint)
-                }
+            //     if (targetReturn == 'org.json.JSONObject'){
+            //         retval.put('gosec!!', taint)
+            //     }
 
-                if (targetReturn == 'org.json.JSONArray'){
-                    retval.put(taint)
-                }
+            //     if (targetReturn == 'org.json.JSONArray'){
+            //         retval.put(taint)
+            //     }
 
 
-            }
+            // }
+            //console.log(retval)
 
 
             //console.log("5==========================================")
             // console.log("\nretval: " + retval);
             // console.warn("\n*** exiting " + targetClassMethod);
-            return retval;
+            
         }
         // 找到并成功hook到了，结束当前循环
     }
@@ -319,6 +442,14 @@ function hook_wechat_content(){
     })
 }
 
+function test(){
+    console.log('==============================')
+    hook('com.mob.tools.utils.Data', '[B', 'AES128Encode', '(java.lang.String,java.lang.String)')
+}
+
+
+
+
 // setTimeout setImmediate
 setTimeout(function () { //prevent timeout
     console.log("[*] Starting script");
@@ -338,8 +469,11 @@ setTimeout(function () { //prevent timeout
     // })
 }, 1000)
 
+
 // export the rpc API
 rpc.exports = {
     hookentry: hook_entry, //导出名不可以有大写字母或者下划线
     getsharecontent: get_share_content,
+    //test:test,
+    //sha256:sha256
 };
